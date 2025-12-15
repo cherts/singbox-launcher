f0633c5d210288d32e969530a8245887219ff34d
Mikhail Grigorev <grigorev_mm@magnit.ru>
2025-12-15 18:05:49 +0500

    Revert

--- core/parsers/node_parser.go
+++ core/parsers/node_parser.go
@@ -48,6 +48,7 @@ func ParseNode(uri string, skipFilters []map[string]string) (*ParsedNode, error)
 
 	// Handle VMess base64 format
 	if strings.HasPrefix(uri, "vmess://") {
+		scheme = "vmess"
 		// VMess is in base64 format, decode with padding support
 		base64Part := strings.TrimPrefix(uri, "vmess://")
 
